bd4618ccd1440614be74f9e8097c4154f76ae9ab
Flavio Pires <flavioasp77@gmail.com>
2022-06-24 14:28:40 -0300

    feat: criada a função ts que adiciona valor ao saldo existente

---
+++ src/desafio-3.ts
@@ -0,0 +1,22 @@
+const soma = document.getElementById('soma') as HTMLInputElement;
+const btnAdd = document.getElementById('somar-ao-saldo') as HTMLButtonElement;
+const saldo = document.getElementById('campo-saldo') as HTMLParagraphElement;
+const btnLimparSaldo = document.getElementById('limpar-saldo') as HTMLButtonElement;
+
+let saldoAtual = 0
+
+function somarAoSaldo(soma: number) {
+  if (saldo) {
+    saldoAtual += soma;
+    saldo.innerHTML = saldoAtual.toString();
+    clean();
+  }    
+}
+
+btnAdd.addEventListener('click', () => {
+  somarAoSaldo(Number(soma.value));
+});
+
+function clean() {
+  soma.value = '';
+}
